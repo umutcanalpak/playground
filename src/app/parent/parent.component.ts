755a90078387c11897fa4a35eeee2353e9c9489c
Umutcan Alpak <umutcanalpak@gmail.com>
2023-04-11 17:32:26 +0300

    rest operator

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -6,7 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
+  arr = [2, 3, 1];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.myFunc('111', '222', '3');
+  }
+
+  myFunc(...arr: string[]) {
+    console.log(111111111);
+
+    for (const item of arr) {
+      console.log(item);
+    }
+    console.log(111111111);
+  }
 }
