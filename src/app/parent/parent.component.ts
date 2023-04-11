8f500699e7d2fd55081bb3d6f41ea08770b7c7b1
Umutcan Alpak <umutcanalpak@gmail.com>
2023-04-11 21:34:57 +0300

    object reference

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MikeService } from '../mike.service';
+import { MikeTwoService } from '../mike2.service';
 
 @Component({
   selector: 'app-parent',
@@ -8,14 +10,57 @@ import { Component } from '@angular/core';
 export class ParentComponent {
   arr = [2, 3, 1];
   arr2 = [999999];
+  obj = {
+    title : 'mike',
+    childObj: {
+      title: 'jesse'
+    }
+  };
 
-  constructor() {}
+  constructor(
+    private mikeService: MikeService,
+    private mikeTwoService: MikeTwoService
+    ) {}
 
   ngOnInit(): void {
-    this.arr2 = this.arr.filter((value: number) => {
 
-      return value === 8;
-    });
+
+    const copy = {
+      ... this.obj
+    };
+
+    console.log(1111111111);
+    console.log(copy.title);
+    console.log(this.obj.title);
+    copy.title = 'asdasdasdasdasd';
+    console.log(copy.title);
+    console.log(this.obj.title);
+    console.log(1111111111);
+
+    console.log(222222222222222);
+    console.log(copy.childObj.title);
+    console.log(this.obj.childObj.title);
+    copy.childObj.title = 'asdasdasdasdasd';
+    console.log(copy.childObj.title);
+    console.log(this.obj.childObj.title);
+    console.log(222222222222222);
+    
+
+
+
+    // console.log(11111);
+    // console.log(this.mikeService.time);
+    // console.log(this.mikeTwoService.time);
+    // console.log(11111);
+    
+
+
+    // this.arr2 = this.arr.filter((value: number) => {
+    //   return value === 8;
+    // });
+
+
+
     // this.myFunc('111', '222', '3');
   }
 
