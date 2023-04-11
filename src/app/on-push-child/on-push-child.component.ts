08ba9949a76235efa12dfbe223fcc8220990e5fb
Umutcan Alpak <umutcanalpak@gmail.com>
2023-04-12 02:32:24 +0300

    on push

--- src/app/on-push-child/on-push-child.component.ts
+++ src/app/on-push-child/on-push-child.component.ts
@@ -1,19 +1,29 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DoCheck,
+  Input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-on-push-child',
   templateUrl: './on-push-child.component.html',
   styleUrls: ['./on-push-child.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OnPushChildComponent implements DoCheck {
   @Input() parentObject: any = {};
   @Input() titleParent = '';
+  time = new Date().getTime();
 
-  constructor(private changeDetectorRef: ChangeDetectorRef) {    
-  }
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   ngDoCheck(): void {
     // this.changeDetectorRef.markForCheck();
+  }
+
+  updateTime() {
+    this.time = new Date().getTime();
   }
 }
