aadd8082d6f7625740d7312c830c8bcaac936277
Umutcan Alpak <umutcanalpak@gmail.com>
2023-04-12 14:28:43 +0300

    viewchild

--- src/app/jesse/jesse.component.ts
+++ src/app/jesse/jesse.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-jesse',
   templateUrl: './jesse.component.html',
-  styleUrls: ['./jesse.component.scss']
+  styleUrls: ['./jesse.component.scss'],
 })
-export class JesseComponent {
+export class JesseComponent implements OnInit, AfterViewInit {
+  @ViewChild('mavi') mavi?: any;
 
+  ngOnInit(): void {
+    console.log(11111111);
+    console.log(this.mavi);
+    console.dir(this.mavi);
+    console.log(11111111);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(222222);
+    console.log(this.mavi);
+    console.dir(this.mavi);
+    console.log(222222);
+  }
 }
