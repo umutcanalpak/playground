0aafe233be64982b63baa875acb7f09674e70e0f
Umutcan Alpak <umutcanalpak@gmail.com>
2023-04-13 11:09:09 +0300

    playground

---
+++ src/assets/playground.js
@@ -0,0 +1,20 @@
+// console.log("**************************");
+
+// function Box(value) {
+//   this.value = value;
+// }
+
+// Box.prototype.age = 33;
+
+// const box1 = new Box("mavi");
+
+
+// console.log(box1.age);
+
+// box1.age = 13213123;
+// // Box.prototype.age = 13213123;
+
+// console.log(box1);
+// console.log(box1.age);
+
+// console.log("**************************");
