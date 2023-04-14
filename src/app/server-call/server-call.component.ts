003a8052a897d9a943e6a7e73b84078620bf99d1
Umutcan Alpak <umutcanalpak@gmail.com>
2023-04-14 13:19:34 +0300

    params

--- src/app/server-call/server-call.component.ts
+++ src/app/server-call/server-call.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -13,17 +13,31 @@ export class ServerCallComponent implements OnInit {
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-    this.httpClient.get(this.url).subscribe({
-      next(value) {
-        console.log(111111111);
-        console.log(value);
-        console.log(111111111);
-      },
-      error(err) {
-        console.log(2222222);
-        console.log(err);
-        console.log(2222222);
-      }
+    const params = new HttpParams({
+      fromString: 'name=term',
     });
+    // const params = new HttpParams({
+    //   fromObject: {
+    //     color: 'mavi',
+    //     age: 33,
+    //   },
+    // });
+
+    this.httpClient
+      .get(this.url, {
+        params,
+      })
+      .subscribe({
+        next(value) {
+          console.log(111111111);
+          console.log(value);
+          console.log(111111111);
+        },
+        error(err) {
+          console.log(2222222);
+          console.log(err);
+          console.log(2222222);
+        },
+      });
   }
 }
