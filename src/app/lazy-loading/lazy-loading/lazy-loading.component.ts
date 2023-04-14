3cfd9e50fc6c747a922b1358b9c08130a361d6d5
Umutcan Alpak <umutcanalpak@gmail.com>
2023-04-15 00:27:18 +0300

    same route navigate scenario

--- src/app/lazy-loading/lazy-loading/lazy-loading.component.ts
+++ src/app/lazy-loading/lazy-loading/lazy-loading.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-lazy-loading',
@@ -6,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lazy-loading.component.scss'],
 })
 export class LazyLoadingComponent implements OnInit {
-  constructor() {
+  constructor(public activatedRoute: ActivatedRoute, public router: Router) {
     console.log('constructor LazyLoadingComponent');
   }
 
   ngOnInit(): void {
     console.log('ngOnInit LazyLoadingComponent');
+  }
+
+  goToRandomId() {
+    const randomNumber = Math.floor(Math.random() * 100);
+
+    this.router.navigate(['main', randomNumber], {
+      relativeTo: this.activatedRoute.parent,
+    });
   }
 }
